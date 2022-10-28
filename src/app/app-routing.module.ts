0d4dec249467a998ead6528218f3ce10a0e2f26f
Gaston <gaston.herrerabaron@outlook.com>
2022-10-28 01:28:22 -0300

    Eliminar guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './components/login/login.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { GuardGuard } from './services/guard.guard';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-/*TODO Revisar GuardGuard  */
 const routes: Routes = [
-  { path: 'portfolio/:username', component: PortfolioComponent /* , canActivate:[GuardGuard] */ },
+  { path: 'portfolio/:username', component: PortfolioComponent },
   { path: 'login', component: LogInComponent },
   { path:'', redirectTo:'portfolio/GastonHB', pathMatch:'full' },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
--- src/app/services/guard.guard.ts
+++ src/app/services/guard.guard.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthenticationService } from './authentication.service';
+import { User } from '../models/user.interface';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GuardGuard implements CanActivate {
-  constructor(private authenticationService:AuthenticationService, private rutas:Router){}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      let currentUser = this.authenticationService.AutenticatedUser;
+  // personId: string = "";
+  nool: boolean = false;
+
+  constructor(private userService: UserService, private rutas:Router) { }
+
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): 
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      // let currentUser = this.authenticationService.AutenticatedUser;
+      const username = route.paramMap.get('username');
+      /* this.route.params.subscribe(params => {
+        const username = params['username']; 
+        this.userService.getByUsername(username).subscribe(user => {
+          this.personId = user.person.id;
+        });
+     }); */
+    if (username) {
+      this.userService.getByUsername(username).subscribe(user => {
+        //this.personId = user.person.id;
+        if (user.person.id) {
+          this.nool =  true;
+        } else {
+          this.nool = false;
       
-      if (currentUser && currentUser.accessToken) {
-        return true;
-      } else {
-        this.rutas.navigate(['login'])
-        return false;
-      }
+        }
+      });
 
+      if (!this.nool) {
+        this.rutas.navigate(['**'])
+      }
+      
+    }
+    return this.nool;
+    
+    
   }
   
 }
